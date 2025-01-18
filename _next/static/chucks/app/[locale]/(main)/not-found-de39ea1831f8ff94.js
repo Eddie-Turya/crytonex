73c0edef4d6d8c46312ea160f238f9925021a3c9
Edwin Turya <164741494+Eddie-Turya@users.noreply.github.com>
2025-01-18 23:21:32 +0300

    Add files via upload

---
+++ _next/static/chucks/app/[locale]/(main)/not-found-de39ea1831f8ff94.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5525,6082,4879],{50677:function(n,e,u){Promise.resolve().then(u.bind(u,2047))}},function(n){n.O(0,[982,8792,2047,2971,8069,1744],function(){return n(n.s=50677)}),_N_E=n.O()}]);
